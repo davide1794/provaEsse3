a0532696950986389d23d5e80ca81a5626aa7821
Davide Amendolare <davide.amendolare@masvis.com>
2017-10-09 13:33:24 +0200

    aggiunta student e index.ts

---
+++ index.ts
@@ -0,0 +1,5 @@
+import {ModuleWithProviders, NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {BrowserModule} from "@angular/platform-browser";
+
+export {Student} from './src/app/student';
